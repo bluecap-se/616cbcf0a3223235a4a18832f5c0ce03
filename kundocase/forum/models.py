c1f46c8d38d4ece1e2eacacbf4e1435b1a7394c6
Andrej Babic <hello@bluecap.se>
2018-10-22 10:39:50 +0200

    Feature: Added date fields to models

--- kundocase/forum/models.py
+++ kundocase/forum/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
+
 class Question(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     user_name = models.CharField(max_length=255)
     user_email = models.EmailField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('created',)
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     user_name = models.CharField(max_length=255)
     user_email = models.EmailField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('created',)
