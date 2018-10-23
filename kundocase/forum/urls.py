b28523a29dadceef59662b45e59d8dbc8718f61f
Andrej Babic <hello@bluecap.se>
2018-10-23 10:00:40 +0200

    Refactor: Merged view functions

--- kundocase/forum/urls.py
+++ kundocase/forum/urls.py
@@ -6,8 +6,8 @@ app_name = 'forum'
 
 urlpatterns = [
     url(r'^$', views.startpage, name='startpage'),
-    url(r'^(?P<id>\d+)$', views.question, name='question'),
-    url(r'^save-question/?$', views.save_question, name='save-question'),
+    url(r'^question/?$', views.question, name='new-question'),
+    url(r'^question/(?P<id>\d+)$', views.question, name='question'),
     url(r'^answer/?$', views.answer, name='new-answer'),
     url(r'^answer/(?P<id>\d+)/?$', views.answer, name='answer'),
 ]
--- kundocase/forum/views.py
+++ kundocase/forum/views.py
@@ -1,7 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.http import HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.decorators.http import require_http_methods
 from kundocase.forum import models, forms
 
 
@@ -14,7 +13,29 @@ def startpage(request):
     })
 
 
-def question(request, id):
+def question(request, id=None):
+    """
+    Question view handler,
+    shows questions and saves new questions
+
+    :param request: Django request object
+    :param id: Optional, `Question` object id
+    :return: Mixed
+    """
+    if request.method == 'POST':
+        data = dict((k, request.POST.get(k, None)) for k, _ in forms.QuestionForm.base_fields.items())
+
+        form = forms.QuestionForm(request.POST)
+        if not form.is_valid():
+            return HttpResponseBadRequest()
+
+        try:
+            question = models.Question(**data)
+            question.save()
+            return redirect('forum:question', id=question.id)
+        except ValidationError as e:
+            return HttpResponseBadRequest(e.message)
+
     question = get_object_or_404(models.Question, id=id)
     answers = question.answer_set.all()
 
@@ -25,25 +46,14 @@ def question(request, id):
     })
 
 
-@require_http_methods(['POST'])
-def save_question(request):
-    data = dict((k, request.POST.get(k, None)) for k, _ in forms.QuestionForm.base_fields.items())
-
-    try:
-        question = models.Question(**data)
-        question.save()
-        return redirect('forum:question', id=question.id)
-    except ValidationError as e:
-        return HttpResponseBadRequest(e.message)
-
-
 def answer(request, id=None):
     """
-
+    Answer view handler,
+    shows answers and saves answers, both new and existing
 
     :param request: Django request object
     :param id: Optional, `Answer` object id
-    :return:
+    :return: Mixed
     """
     if request.method == 'POST':
         data = dict((k, request.POST.get(k, None)) for k, _ in forms.AnswerForm.base_fields.items())
