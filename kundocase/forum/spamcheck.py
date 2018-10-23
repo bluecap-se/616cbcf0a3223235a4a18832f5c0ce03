897be0836717d9fdebcf82ec2998d9ef5ba2a6fa
Andrej Babic <hello@bluecap.se>
2018-10-23 13:26:55 +0200

    Refactor: Updated spamcheck module

--- kundocase/forum/spamcheck.py
+++ kundocase/forum/spamcheck.py
@@ -1,82 +1,60 @@
 import requests
 
-def spamcheck_question(question):
-    is_spam = long_texts_are_spam(question)
 
-    if not is_spam:
-        is_spam = long_titles_are_spam(question)
-    if not is_spam:
-        is_spam = all_users_from_domain_are_spam(question)
-    if not is_spam:
-        is_spam = some_usernames_are_spam(question)
-    if not is_spam:
-        is_spam = words_in_text_are_spam(question)
-    if not is_spam:
-        is_spam = known_spammers_are_spam(question)
+class SpamCheck:
 
-    return is_spam
+    def __init__(self, object):
+        self.obj = object
 
-def spamcheck_answer(answer):
-    is_spam = long_texts_are_spam(answer)
+    def has_spam(self):
+        """
+        Validates if object contains spam
 
-    if not is_spam:
-        is_spam = all_users_from_domain_are_spam(answer)
-    if not is_spam:
-        is_spam = some_usernames_are_spam(answer)
-    if not is_spam:
-        is_spam = words_in_text_are_spam(answer)
-    if not is_spam:
-        is_spam = known_spammers_are_spam(answer)
+        :param object: Django model object, of `Question` or `Answer` type
+        :return: Boolean, True if object has spam
+        """
+        return self.long_texts_are_spam() or \
+               self.long_titles_are_spam() or \
+               self.all_users_from_domain_are_spam() or \
+               self.some_usernames_are_spam() or \
+               self.words_in_text_are_spam() or \
+               self.known_spammers_are_spam()
 
-    return is_spam
+    def long_texts_are_spam(self):
+        return len(self.obj.content) > 200
 
-# Different ways to check for spam
-def long_texts_are_spam(question_or_comment):
-    if len(question_or_comment.content) > 200:
-        return True
+    def long_titles_are_spam(self):
+        return hasattr(self.obj, 'title') and len(self.obj.title) > 200
 
-    return False
+    def all_users_from_domain_are_spam(self):
+        bad_domains = ["spam.com", "universitydiploma.com"]
+        domain = self.obj.user_email.split("@")[1]
+        return domain in bad_domains
 
-# Different ways to check for spam
-def long_titles_are_spam(question_or_comment):
-    if len(question_or_comment.title) > 200:
-        return True
+    def some_usernames_are_spam(self):
+        bad_names = ["thord", "curt", "madicken"]
+        name = self.obj.user_name
 
-    return False
+        for bad_name in bad_names:
+            if name in bad_name:
+                return True
 
-def all_users_from_domain_are_spam(question_or_comment):
-    bad_domains = ["spam.com", "universitydiploma.com"]
-    domain = question_or_comment.user_email.split("@")[1]
-    if domain in bad_domains:
-        return True
+        return False
 
-    return False
+    def words_in_text_are_spam(self):
+        bad_words = ["spam", "universitydiploma"]
+        text = self.obj.content
 
-def some_usernames_are_spam(question_or_comment):
-    bad_names = ["thord", "curt", "madicken"]
-    name = question_or_comment.user_name
-    for bad_name in bad_names:
-        if name in bad_name:
-            return True
+        for word in bad_words:
+            if word in text:
+                return True
 
-    return False
+        return False
 
-def words_in_text_are_spam(question_or_comment):
-    bad_words = ["spam", "universitydiploma"]
-    text = question_or_comment.content
-    for word in bad_words:
-        if word in text:
-            return True
+    def known_spammers_are_spam(self):
+        email = self.obj.user_email
+        url = "https://www.stopforumspam.com/api?f=json&email=" + email
+        response = requests.get(url, timeout=2)
+        data = response.json()
 
-    return False
-
-def known_spammers_are_spam(question_or_comment):
-    email = question_or_comment.user_email
-    url = "https://www.stopforumspam.com/api?f=json&email=" + email
-    response = requests.get(url, timeout=2)
-    data = response.json()
-
-    if data and data["success"] and data["email"]["appears"]:
-        return True
-
-    return False
+        return bool(data and data["success"] and data["email"]["appears"])
